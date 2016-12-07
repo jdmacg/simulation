a75e37df0ea8b37508b5627b238e0897b308ca18
jdmacg <j.mcgregor@queensu.ca>
2016-12-06 19:01:05 -0500

    Add encrypted password column

--- db/migrate/20161206223739_create_users.rb
+++ db/migrate/20161206223739_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
     	t.string "username"
     	t.string "password"
+    	t.string "encrypted_password"
+    	t.string "salt"
       t.timestamps
     end
   end
