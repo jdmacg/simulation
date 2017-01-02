9be86d978e0541cb6481506809be7e2c815d5836
graham <graham.sm94@gmail.com>
2017-01-02 18:15:39 -0500

    Updated property view style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  has_one :team
-  #
+  # has_one :team
+  has_many :properties
+  has_many :trade_requests
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   # #Only on Create so other actions like update password attribute can be nil
   # validates_length_of :password, :in => 6..20, :on => :create
-
-
 end
