585eb3fc0ba5101ad157dc52ec064ec9994b4c45
jdmacg <j.mcgregor@queensu.ca>
2017-01-19 01:14:03 -0500

    Update multiplication

--- app/controllers/admin/properties_controller.rb
+++ app/controllers/admin/properties_controller.rb
@@ -9,13 +9,13 @@ class Admin::PropertiesController < Admin::ApplicationController
   	Property.all.each do |prop|
   	  if prop.developed == false
   	  	if prop.province == 'Alberta'
-  	  	  prop.value *= 0.9
+  	  	  prop.value = prop.value * 0.9
   	  	  prop.save!
   	  	elsif prop.province = 'Quebec'
-  	  	  prop.value *= 0.9
+  	  	  prop.value = prop.value *  0.9
   	  	  prop.save!
   	  	else
-  	  	  prop.value *= 1.03
+  	  	  prop.value = prop.value * 1.03
   	  	  prop.save!
   	  	end
       end
@@ -28,23 +28,23 @@ class Admin::PropertiesController < Admin::ApplicationController
   	Property.all.each do |prop|
   	  if prop.developed == false
   	  	if prop.city == 'Toronto'
-  	  	  prop.value *= 1.1
+  	  	  prop.value = prop.value * 1.1
   	  	  prop.save!
   	  	elsif prop.city = 'Vancouver'
-  	  	  prop.value *= 1.1
+  	  	  prop.value = prop.value * 1.1
   	  	  prop.save!
   	  	end
   	  	if prop.property_type_id == 1
-  	  	  prop.value *= 1.08
+  	  	  prop.value = prop.value * 1.08
   	  	  prop.save!
   	  	elsif prop.property_type_id == 2
-  	  	  prop.value *= 1.12
+  	  	  prop.value = prop.value * 1.12
   	  	  prop.save!
   	  	elsif prop.property_type_id == 3
-  	  	  prop.value *= 1.1
+  	  	  prop.value = prop.value *1.1
   	  	  prop.save!
   	  	elsif prop.property_type_id == 4
-  	  	  prop.value *= 1.15
+  	  	  prop.value = prop.value * 1.15
   	  	  prop.save!
   	  	end
       end
@@ -56,7 +56,7 @@ class Admin::PropertiesController < Admin::ApplicationController
   def shock3
   	Property.all.each do |prop|
   	  if prop.developed == false
-  	  	prop.value *= 0.87
+  	  	prop.value = prop.value * 0.87
   	  	prop.save!
   	  end
   	end
@@ -67,30 +67,30 @@ class Admin::PropertiesController < Admin::ApplicationController
   	Property.all.each do |prop|
   	  if prop.developed == false
   	  	if prop.city == 'Toronto'
-  	  	  prop.value *= 1.16
+  	  	  prop.value = prop.value * 1.16
   	  	  prop.save!
   	  	elsif prop.city = 'Vancouver'
-  	  	  prop.value *= 1.16
+  	  	  prop.value = prop.value * 1.16
   	  	  prop.save!
   	  	end
   	  	if prop.province == 'Alberta'
-  	  	  prop.value *= 1.22
+  	  	  prop.value = prop.value * 1.22
   	  	  prop.save!
   	  	elsif prop.province == 'Quebec'
-  	  	  prop.value *= 1.22
+  	  	  prop.value = prop.value * 1.22
   	  	  prop.save!
   	  	end
   	  	if prop.property_type_id == 1
-  	  	  prop.value *= 1.03
+  	  	  prop.value = prop.value * 1.03
   	  	  prop.save!
   	  	elsif prop.property_type_id == 2
-  	  	  prop.value *= 1.04
+  	  	  prop.value = prop.value * 1.04
   	  	  prop.save!
   	  	elsif prop.property_type_id == 3
-  	  	  prop.value *= 1.05
+  	  	  prop.value = prop.value * 1.05
   	  	  prop.save!
   	  	elsif prop.property_type_id == 4
-  	  	  prop.value *= 1.07
+  	  	  prop.value = prop.value * 1.07
   	  	  prop.save!
   	  	end
       end
